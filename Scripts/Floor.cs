d4397be31ec61c4e822fcd7d2f593031568392e2
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-05-14 11:48:27 +0200

    Set Elevator origin to floor

--- Scripts/Floor.cs
+++ Scripts/Floor.cs
@@ -7,5 +7,6 @@ namespace ElevatorTask
     public class Floor : MonoBehaviour
     {
         [field: SerializeField] public Animator DoorsAnimator { get; private set; }
+        [field: SerializeField] public Transform ElevatorTarget { get; private set; }
     }
 }
