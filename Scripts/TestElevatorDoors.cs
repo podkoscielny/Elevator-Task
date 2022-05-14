5761f456f4485c704ebab679599d10a93d1ae22b
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-05-14 10:43:06 +0200

    Add floor doors open and close animation

--- Scripts/TestElevatorDoors.cs
+++ Scripts/TestElevatorDoors.cs
@@ -7,16 +7,19 @@ namespace ElevatorTask
     public class TestElevatorDoors : MonoBehaviour
     {
         [SerializeField] Animator elevatorAnimator;
+        [SerializeField] Animator floorDoorsAnimator;
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.C))
             {
                 elevatorAnimator.SetTrigger("Close");
+                floorDoorsAnimator.SetTrigger("Close");
             }
             else if (Input.GetKeyDown(KeyCode.O))
             {
                 elevatorAnimator.SetTrigger("Open");
+                floorDoorsAnimator.SetTrigger("Open");
             }
         }
     }
